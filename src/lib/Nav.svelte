5493eda0a1f8a2a9801c30fc316c6036e4e44e7e
Mountaser164 <9015678@student.zadkine.nl>
2026-03-25 11:16:51 +0100

    fix anchor links

--- src/lib/Nav.svelte
+++ src/lib/Nav.svelte
@@ -1,43 +1,44 @@
 <script lang="ts">
 	import { viewState } from './store.svelte';
+    import { base } from '$app/paths';
 </script>
 
 <!-- Sidebar rechts -->
 <aside class="w-68 flex-none overflow-y-auto bg-gray-50 p-4" style="font-family: 'Barlow Condensed', sans-serif;">
 	<h2 class="mb-8 text-lg font-bold">Kaartcollectie</h2>
 	<ul class="flex flex-col divide-y divide-gray-200">
-    <li>
-    <a href="/" class="flex items-center gap-3 bg-gray-800 px-3 py-2 text-white hover:bg-gray-700">
+   <li>
+    <a href="{base}/" class="flex items-center gap-3 bg-gray-800 px-3 py-2 text-white hover:bg-gray-700">
         <span class="text-xs font-bold text-orange-400">1850</span>
         <span class="font-semibold">Kadasterkaart</span>
     </a>
 </li>
 <li>
-    <a href="/" class="flex items-center gap-3 px-3 py-2 hover:bg-gray-200">
+    <a href="{base}/" class="flex items-center gap-3 px-3 py-2 hover:bg-gray-200">
         <span class="text-xs font-bold text-orange-400">1890</span>
         <span>Stadsuitbreiding</span>
     </a>
 </li>
 <li>
-    <a href="/" class="flex items-center gap-3 px-3 py-2 hover:bg-gray-200">
+    <a href="{base}/" class="flex items-center gap-3 px-3 py-2 hover:bg-gray-200">
         <span class="text-xs font-bold text-orange-400">1920</span>
         <span>Topografische kaart</span>
     </a>
 </li>
 <li>
-    <a href="/" class="flex items-center gap-3 px-3 py-2 hover:bg-gray-200">
+    <a href="{base}/" class="flex items-center gap-3 px-3 py-2 hover:bg-gray-200">
         <span class="text-xs font-bold text-orange-400">1940</span>
         <span>Vóór bombardement</span>
     </a>
 </li>
 <li>
-    <a href="/" class="flex items-center gap-3 px-3 py-2 hover:bg-gray-200">
+    <a href="{base}/" class="flex items-center gap-3 px-3 py-2 hover:bg-gray-200">
         <span class="text-xs font-bold text-blue-500">1960</span>
         <span>Wederopbouw</span>
     </a>
 </li>
 <li>
-    <a href="/" class="flex items-center gap-3 px-3 py-2 hover:bg-gray-200">
+    <a href="{base}/" class="flex items-center gap-3 px-3 py-2 hover:bg-gray-200">
         <span class="text-xs font-bold text-blue-500">1980</span>
         <span>Moderne atlas</span>
     </a>
