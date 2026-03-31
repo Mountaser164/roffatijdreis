fd79017943312cf5d5b426fcf7f047db5fe0f90a
Mountaser164 <9015678@student.zadkine.nl>
2026-03-31 19:47:21 +0200

    fix import case sensitivity

--- src/lib/models/Comparison.ts
+++ src/lib/models/Comparison.ts
@@ -1,23 +1,23 @@
-export class Comparison {
-    active: boolean = $state(false);
-    leftAnnotation: string = $state('');
-    rightAnnotation: string = $state('');
+// export class Comparison {
+//     active: boolean = $state(false);
+//     leftAnnotation: string = $state('');
+//     rightAnnotation: string = $state('');
 
-    enable(): void {
-        this.active = true;
-    }
+//     enable(): void {
+//         this.active = true;
+//     }
 
-    disable(): void {
-        this.active = false;
-    }
+//     disable(): void {
+//         this.active = false;
+//     }
 
-    setLeft(annotation: string): void {
-        this.leftAnnotation = annotation;
-    }
+//     setLeft(annotation: string): void {
+//         this.leftAnnotation = annotation;
+//     }
 
-    setRight(annotation: string): void {
-        this.rightAnnotation = annotation;
-    }
-}
+//     setRight(annotation: string): void {
+//         this.rightAnnotation = annotation;
+//     }
+// }
 
-export const comparison = new Comparison();
+// export const comparison = new Comparison();
